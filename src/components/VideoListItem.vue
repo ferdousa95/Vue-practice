df42a18437062231b4b571dd25d34bb5b83d2f5b
adit95 <ferdousadit99@gmail.com>
2023-10-22 18:27:12 -0600

    added css to VideoListItem

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,7 +1,9 @@
 <template>
     <li class="list-group-item">
-        <img v-bind:src="thumbnailUrl" />
-        {{ video.snippet.title }}
+        <img class="mr-3" v-bind:src="thumbnailUrl" />
+        <div class = "media-body">
+            {{ videoTitle }}
+        </div>
     </li>
 </template>
 
@@ -14,7 +16,20 @@ export default {
     computed: {
         thumbnailUrl() {
             return this.video.snippet.thumbnails.default.url;
+        },
+        videoTitle() {
+            return this.video.snippet.title;
         }
     }
 }
 </script>
+
+<style scoped>
+    li {
+        display: flex;
+        cursor: pointer;
+    }
+    li:hover {
+        background-color: #eee;
+    }
+</style>
